21f2828376bcee123fbc6333463d5d29c57cc577
ersajo <sanchezjoserick@gmail.com>
2017-04-21 10:01:49 -0500

    Pruebas para elevar una potencia recursivo

--- DH/DH_Client.py
+++ DH/DH_Client.py
@@ -2,6 +2,14 @@ import sys
 from random import random, randrange
 import socket
 
+def exp(num, exp, res, mod):
+    if exp == 0:
+        return res
+    else:
+        res = (res * num) % mod
+        exp -= 1
+        exp(num, exp, res, mod)
+
 def es_primo(num):
     i = 1;
     if num < 2:
@@ -19,15 +27,11 @@ def genP():
         if es_primo(num):
             return num
 
-def genA():
-    while True:
-        num = randrange(int(p))
-        if es_primo(num):
-            return num
-
 p = genP()
 p = str(p)[0:8]
-a = genA()
+a = num = randrange(int(p))
+ka = exp(3,4,1,5)
+print ka
 
 client = socket.socket()
 client.connect(('127.0.0.1', 7000))
--- DH/DH_Server.py
+++ DH/DH_Server.py
@@ -2,22 +2,6 @@ import sys
 from random import randrange
 import socket
 
-def es_primo(num):
-    i = 1;
-    if num < 2:
-        return False
-    while num % i != 0:
-        i += 1
-        if num % i == 0:
-            return False
-    return True
-
-def genB():
-    while True:
-        num = randrange(int(P))
-        if es_primo(num):
-            return num
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 7000))
 server.listen(1)
@@ -25,7 +9,7 @@ client, addr = server.accept()
 
 P = client.recv(8)
 print "P: " + P
-b = genB()
+b = num = randrange(int(P))
 print "B: " + str(b)
 
 while True:

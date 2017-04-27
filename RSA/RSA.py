195747a7994397725e86c9386992ae6a7a2a1c6a
ersajo <sanchezjoserick@gmail.com>
2017-04-26 23:55:05 -0500

    Calculo primos y modulo n

--- RSA/RSA.py
+++ RSA/RSA.py
@@ -1,1 +1,39 @@
 import sys
+from random import random, randrange
+
+def exp(base, expo, mod):
+    ans = 1
+    if expo == 0:
+        ans = 1
+    else:
+        while expo >= 1:
+            ans *= base
+            expo -= 1
+            ans = ans % mod
+        return ans
+
+def es_primo(num):
+    i = 1;
+    if num < 2:
+        return False
+    while num % i != 0:
+        i += 1
+        if num % i == 0:
+            return False
+    return True
+
+def genP():
+    while True:
+        ran = str(random()).split('.')
+        num = int(ran[1])
+        if es_primo(num):
+            return num
+
+p = genP()
+q = genP()
+
+n = p*q;
+
+print "p: " + str(p)
+print "q: " + str(q)
+print "n: " + str(n)

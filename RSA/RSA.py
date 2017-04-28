9a26541824af777d804e2bfbd88dfacc75e45e82
ersajo <sanchezjoserick@gmail.com>
2017-04-28 17:25:55 -0500

    Agregar llaves de usuario

--- RSA/RSA.py
+++ RSA/RSA.py
@@ -1,57 +1,43 @@
-from random import random, randrange
+import os
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
 
-def llavePublica():
-    bits=2048
-    new_key = RSA.generate(bits, e=65537)
-    public_key = new_key.publickey().exportKey("PEM")
-    llavePublica = open("llavePublicaCandy.txt", "w")
-    llavePublica.write(public_key)
+def generate_RSA(name):
+    new_key = RSA.generate(bits=2048, e=65537)
+
+    publickey = new_key.publickey().exportKey("PEM")
+    llavePublica = open(name + "Public.txt", "w")
+    llavePublica.write(publickey)
     llavePublica.close()
 
-def llavePrivada():
-    bits=2048
-    new_key = RSA.generate(bits, e=65537)
-    public_key = new_key.publickey().exportKey("PEM")
-    private_key = new_key.exportKey("PEM")
-    llavePrivada = open("llavePrivadaCandy.txt","w")
-    llavePrivada.write(private_key)
+    privatekey = new_key.exportKey("PEM")
+    llavePrivada = open(name + "Private.txt", "w")
+    llavePrivada.write(privatekey)
     llavePrivada.close()
 
-def es_primo(num):
-    i = 1;
-    if num < 2:
-        return False
-    while num % i != 0:
-        i += 1
-        if num % i == 0:
-            return False
-    return True
+def menu():
+    os.system('clear')
+    print "1. Generate keys."
+    print "2. Encrypt file"
+    print "3. Decrypt file"
+    print "q. Quit"
 
-def genP():
-    while True:
-        ran = str(random()).split('.')
-        num = int(ran[1])
-        if es_primo(num):
-            return num
-def gcd(a,b):
-    while b != 0:
-        (a,b) = (a, a % b)
-    return a;
-
-def genE():
-    p = genP()
-    q = genP()
-    n = p*q
-    phi = (p-1)*(q-1)
-    e = randrange(n)
-    while(True):
-        if(gcd(e,phi) == 1):
-            break
-        e = randrange(n)
-        print e
-    return e
-
-llavePrivada()
-llavePublica()
+while True:
+    menu()
+    optMenu = raw_input ("Choose an option >> ")
+    if optMenu == "1":
+        name = raw_input("Enter the name to the keys \n>>")
+        print "Generating keys"
+        generate_RSA(name)
+        print "Done!"
+        raw_input("Press enter to continue")
+    elif optMenu == "2":
+        print "Encrypt file"
+        raw_input("Press enter to continue")
+    elif optMenu == "3":
+        print "Decrypt file"
+        raw_input("Press enter to continue")
+    elif optMenu == "q":
+        print "Bye"
+        break
+    else:
+        raw_input("Wrong option...\nPress enter to continue")   #Opcion erronea
